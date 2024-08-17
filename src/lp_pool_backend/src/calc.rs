9f97313cd2b2e035c7d42345059ccd175863d3cc
goracy <jakubgoracy@student.agh.edu.pl>
2024-08-17 17:09:20 +0200

    lp struct impl

---
+++ src/lp_pool_backend/src/calc.rs
@@ -0,0 +1,14 @@
+// 1_000_000 standard for most common tokens on Solana
+// even Solana have 1_000_000_000
+const PRECISION: u64 = 1_000_000;
+
+pub fn multiply(a: u64, b: u64) -> u64 {
+    let result = a * b;
+    result / PRECISION
+}
+
+pub fn divide(a: u64, b: u64) -> u64 {
+    let a_scale = a * PRECISION;
+    let result = a_scale / b;
+    result
+}
---
+++ src/lp_pool_backend/src/errors.rs
@@ -0,0 +1,10 @@
+#[derive(Debug)]
+pub enum Errors {
+    InvalidPrice,
+    InvalidFee,
+    InvalidLiquidityTarget,
+    InvalidDeposit,
+    InvalidSwapAmount,
+    InvalidLpTokenToRemove,
+    ToBigSwap(u64),
+}
